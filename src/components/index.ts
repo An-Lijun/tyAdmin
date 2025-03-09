a16a939c8f364fb21bc7015b9dfa9ffa8551b4c6
yutian <nj_yutian@qq.com>
2025-03-09 15:04:36 +0800

    feat 增加mock toyarDesign

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,11 @@
 import type { App } from 'vue';
 // @ts-ignore
-import 'toyar-design/dists/index2.js'
+import 'toyar-design/dist/index2.js'
 
 // import toyar from 'toyar-design/dist/index.js'
 // import 'toyar-design/dist/style.css'
 import toyar from './dists/index.js'
-import './dist/style.css'
+import './dists/style.css'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 export default function installComponents(app: App<Element>) {
