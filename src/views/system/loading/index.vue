153e85400ae4a0c9fc2d04c5eae88e6840c80f7f
yutian <nj_yutian@qq.com>
2025-03-21 20:06:46 +0800

    feat 给全局loading添加过渡

--- src/views/system/loading/index.vue
+++ src/views/system/loading/index.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="load-container"  v-show="loading">
-    <div class="boxLoading">
+  <transition name="loading">
+    <div class="load-container" v-show="loading">
+      <div class="boxLoading">
+      </div>
+      <span class="loadingText">Toyar Admin</span>
     </div>
-    <span class="loadingText">Toyar Admin</span>
-  </div>
-   <slot/>
+  </transition>
+  <slot />
 </template>
 <script setup>
 
 const props = defineProps({
-  loading:{
-    default:false
+  loading: {
+    default: false
   }
 })
 </script>
 <style lang="scss" scoped>
 @import url('/static/loading.css');
+
+/* 定义过渡类名 */
+.loading-enter-active,
+.loading-leave-active {
+  transition: opacity 1.5s;
+}
+
+.loading-enter,
+.loading-leave-to {
+  opacity: 0;
+}
 </style>
